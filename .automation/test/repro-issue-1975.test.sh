7b5fe3d4bee2d558db084db1d87448bb8171cfa8
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-06-15 20:10:38 +0200

    HACK: TEST SCRIPT

--- .automation/test/repro-issue-1975.test.sh
+++ .automation/test/repro-issue-1975.test.sh
@@ -19,47 +19,47 @@ rebuild_image() {
     .
 }
 
-git revert --no-edit "$commit"
-
-# before
-make bootstrap
-rebuild_image
-prefix="before-change"
-
-# normal mode: run with normal megalinter code
+#git revert --no-edit "$commit"
 #
-# result:
-# - files created (not changed) by megalinter are owned by root
-MEGALINTER_IMAGE="$image" \
-MEGALINTER_NO_DOCKER_PULL=true \
-.automation/test/repro-issue-1975.sh \
-  &>".automation/test/repro-issue-1975.${prefix}.normal.log"
-
-# docker-user mode: runs docker directly with
-# `--user "$(id -u):$(id -g)"`
-# to test
+## before
+#make bootstrap
+#rebuild_image
+#prefix="before-change"
 #
-# result:
-# - files created (not changed) by megalinter are owned by host user
-# - error: could not lock config file //.gitconfig: Permission denied
-MEGALINTER_IMAGE="$image" \
-.automation/test/repro-issue-1975.sh --docker-user \
-  &>".automation/test/repro-issue-1975.${prefix}.docker-user.log"
-
-# docker-user mode: runs docker directly with
-# `--user "$(id -u):$(id -g)"`
-# and a writable user home
-# to test
+## normal mode: run with normal megalinter code
+##
+## result:
+## - files created (not changed) by megalinter are owned by root
+#MEGALINTER_IMAGE="$image" \
+#MEGALINTER_NO_DOCKER_PULL=true \
+#.automation/test/repro-issue-1975.sh \
+#  &>".automation/test/repro-issue-1975.${prefix}.normal.log"
 #
-# result:
-# - files created (not changed) by megalinter are owned by host user
-# - no permission denied error
-MEGALINTER_IMAGE="$image" \
-.automation/test/repro-issue-1975.sh --docker-user-home \
-  &>".automation/test/repro-issue-1975.${prefix}.docker-user-home.log"
-
-# add change
-git cherry-pick "$commit"
+## docker-user mode: runs docker directly with
+## `--user "$(id -u):$(id -g)"`
+## to test
+##
+## result:
+## - files created (not changed) by megalinter are owned by host user
+## - error: could not lock config file //.gitconfig: Permission denied
+#MEGALINTER_IMAGE="$image" \
+#.automation/test/repro-issue-1975.sh --docker-user \
+#  &>".automation/test/repro-issue-1975.${prefix}.docker-user.log"
+#
+## docker-user mode: runs docker directly with
+## `--user "$(id -u):$(id -g)"`
+## and a writable user home
+## to test
+##
+## result:
+## - files created (not changed) by megalinter are owned by host user
+## - no permission denied error
+#MEGALINTER_IMAGE="$image" \
+#.automation/test/repro-issue-1975.sh --docker-user-home \
+#  &>".automation/test/repro-issue-1975.${prefix}.docker-user-home.log"
+#
+## add change
+#git cherry-pick "$commit"
 
 # after
 make bootstrap
